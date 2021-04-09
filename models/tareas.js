ed2de7fc744196e32a51c04c7e4ad034c2b11d1c
Jose B <josebolanoslanzarote@gmail.com>
2021-04-09 12:43:55 +0100

    1 proyect

---
+++ models/tareas.js
@@ -0,0 +1,21 @@
+const tarea = require('./tarea')
+
+class Tareas {
+
+
+
+    constructor() {
+
+        this._listado = {};
+        
+    }
+
+    crearTarea( desc = '' ) {
+
+        const tarea = new Tarea(desc);
+        this._listado[tarea.id] = tarea;
+    }
+}
+
+
+module.exports = Tareas;
